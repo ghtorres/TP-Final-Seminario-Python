061d3eb11d36ae4bc35bb5b3a32ee013b23208a2
Gastón Hernán Torres <ghtorres92@gmail.com>
2019-07-19 12:17:39 -0300

    Create funciones_muestra_temperatura.py

---
+++ Raspberry Pi/funciones_muestra_temperatura.py
@@ -0,0 +1,31 @@
+
+from matriz import Matriz
+from sonido import Sonido
+from temperatura import Temperatura
+import time
+
+# Conexión de los sensores en sus respectivos pines
+# Matriz --> vcc: 2, gnd: 6, din: 19, cs: 24, clk: 23
+# Sonido --> a0: 7, gnd: 9, vc: 3, d0: 15
+# Temperatura --> vcc: 1, sda: 11, clk: 14
+
+# Activamos los sensores que vamos a usar
+# matriz = Matriz(numero_matrices=2, ancho=16)
+matriz = Matriz(numero_matrices=2, ancho=16)
+sonido = Sonido()
+temperatura = Temperatura()
+
+
+def acciones():
+    print("Sonido Detectado!")
+    temp_data = temperatura.datos_sensor()
+    temp_formateada = 'Temperatura = {0:0.1f}°C  Humedad = {1:0.1f}%'.format(temp_data['temperatura'],
+                                                                             temp_data['humedad'])
+    matriz.mostrar_mensaje(temp_formateada, delay=0.08, font=2)
+
+
+def mostrar():
+    time.sleep(0.1)
+    print('ejecutar mostrar')
+    sonido.evento_detectado(acciones)
+    
